c1ab356c5d816854e9258717e6a4acb38861089e
kyrylo prymak <prymak.kyrylo@gmail.com>
2026-01-30 16:27:51 +0100

    Basic version of webinterface with additional parameters included in the API

--- app.py
+++ app.py
@@ -1,48 +1,31 @@
-import os
 import streamlit as st
+import requests
+import base64
 
 
-# Define the base URI of the API
-#   - Potential sources are in `.streamlit/secrets.toml` or in the Secrets section
-#     on Streamlit Cloud
-#   - The source selected is based on the shell variable passend when launching streamlit
-#     (shortcuts are included in Makefile). By default it takes the cloud API url
-#if 'API_URI' in os.environ:
-#    BASE_URI = st.secrets[os.environ.get('API_URI')]
-#else:
-#    BASE_URI = st.secrets['cloud_api_uri']
-# Add a '/' at the end if it's not there
-#BASE_URI = BASE_URI if BASE_URI.endswith('/') else BASE_URI + '/'
-# Define the url to be used by requests.get to get a prediction (adapt if needed)
-#url = BASE_URI + 'predict'
+#Title
+st.title("Personal Shopping Assistant")
 
-# Just displaying the source for the API. Remove this in your final version.
-#st.markdown(f"Working with {url}")
+#Image link and search
+with st.form("search_form"):
+    url = st.text_input("Paste an image URL:")
+    submitted = st.form_submit_button("Search")
 
-st.markdown("Now, the rest is up to you. Start creating your page.")
+if submitted:
 
+    #Display the input image
+    st.image(url, caption="Your image", width=300)
+    response = requests.get("https://api-520917056692.europe-west1.run.app/predict",
+                            params={"image_path": url, "top_k": 5})
 
-# TODO: Add some titles, introduction, ...
-
-
-# TODO: Request user input
-
-
-# TODO: Call the API using the user's input
-#   - url is already defined above
-#   - create a params dict based on the user's input
-#   - finally call your API using the requests package
-
-
-# TODO: retrieve the results
-#   - add a little check if you got an ok response (status code 200) or something else
-#   - retrieve the prediction from the JSON
-
-
-# TODO: display the prediction in some fancy way to the user
-
-
-# TODO: [OPTIONAL] maybe you can add some other pages?
-#   - some statistical data you collected in graphs
-#   - description of your product
-#   - a 'Who are we?'-page
+    if response.status_code == 200:
+        st.markdown("### Recommendations:")
+        results = response.json()
+        cols = st.columns(len(results))
+        for col, img in zip(cols, results):
+            col.image(base64.b64decode(img["data"]), caption=img["name"])
+            col.markdown(f"**{img.get('description', '')}**")
+            col.text(f"Category: {img.get('subcategory', 'Unknown')}")
+            col.text(f"Gender: {img.get('gender', 'Unknown')}")
+    else:
+        st.error("API error")
